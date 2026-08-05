e6f3321cc711ef0a976dd474aa311cfa2dc3c5cf
Max <simulacronzen@proton.me>
2026-08-05 16:48:43 +0200

    WIP : sorties/villes fixtures connexions

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class UserController extends AbstractController
 {
     #[Route('/', name: 'app_user')]
-    public function index(): Response
+    public function index(SortieRepository $sortieRepository): Response
     {
+        $sorties = $sortieRepository->findAll();
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'sorties' => $sorties,
         ]);
     }
 }
