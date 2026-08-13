55e7ff8560133fa19bb236e3a282458c113fa45a
Salomé Marti <salome.marti2026@campus-eni.fr>
2026-08-13 09:41:34 +0200

    fix: modification de l'affichage des sorties et de la route de modfication

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Enum\Etat;
 use App\Form\SortieType;
 use App\Repository\CampusRepository;
-use App\Repository\LieuRepository;
 use App\Repository\SortieRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
@@ -88,39 +88,96 @@ final class SortieController extends AbstractController
         ]);
     }
 
-    #[Route('/sortie/create', name: 'app_sortie_create', methods: ['GET', 'POST'])]
-    public function create(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        LieuRepository $lieuRepository
-    ): Response {
-        $user = $this->getUser();
-        $sortie = new Sortie();
-        $sortie->setOrganisateur($user);
-        $sortie->setEtat(Etat::EnCreation);
-
-        $form = $this->createForm(SortieType::class, $sortie);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($sortie);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user');
-        }
-
-        $lieux = $lieuRepository->findAll();
-
-        return $this->render('sortie/create.html.twig', [
-            'form' => $form,
-            'lieux' => $lieux,
-        ]);
-    }
-
     #[Route('/sortie/{id}', name: 'app_sortie_detail')]
     public function detail(Sortie $sortie): Response
     {
         return $this->render('sortie/detail.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+    #[Route('/sortie/create', name: 'app_sortie_create', methods: ['GET', 'POST'])]
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ): Response {
+        $user = $this->getUser();
+
+        if (!$user instanceof Participant) {
+            throw $this->createAccessDeniedException('Vous devez être connecté.');
+        }
+        $sortie = new Sortie();
+        $sortie->setOrganisateur($user);
+        $sortie->setCampus($user->getCampus());
+        $sortie->setEtat(Etat::EnCreation);
+
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            $clickedAction = $request->request->get('action');
+            $sortie->setEtat($clickedAction === 'publier' ? Etat::Ouverte : Etat::EnCreation);
+
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('sucess', $clickedAction === 'publier'
+                ? 'Sortie publiée avec succès!'
+                : 'Sortie enregistrée en tant que brouillon.');
+
+            return $this->redirectToRoute('app_sortie_detail', ['id' => $sortie->getId()]);
+        }
+        return $this->render('sortie/create.html.twig', [
+            'sortieForm' => $sortieForm,
+            'sortie' => $sortie,
+        ]);
+    }
+
+    #[Route('/sortie/{id}/update', name: 'app_sortie_update', requirements: ['id' => '\d+'], methods: ['GET','POST'])]
+    public function update(
+        Sortie $sortie,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        /** @var Participant|null $participant */
+        $participant = $this->getUser();
+
+        //vérifier si 'utilisateur est bien connecté
+        if (!$participant instanceof Participant) {
+            throw $this->createAccessDeniedException('Vous devez être connecté');
+        }
+
+        //seul l'organisateur peut modifier la sortie et seulement tant que la sortie n'est pas publiée
+        if ($sortie->getOrganisateur() !== $participant) {
+            throw $this->createAccessDeniedException('Vous n\'êtes pas l\'organisateur de cette sortie.');
+        }
+        if ($sortie->getEtat() !== Etat::EnCreation) {
+            $this->addFlash('error', 'Cette sortie n\'est plus modifiable.');
+            return $this->redirectToRoute('app_sortie_detail', ['id' => $sortie->getId()]);
+        }
+
+        //le formulaire associé à l'entité vide
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        //récupération des données du form et les injecte dans la $sortie
+        $sortieForm->handleRequest($request);
+
+        //si le formulaire est soumis et valide
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            $clickedAction = $request->request->get('action');
+            $sortie->setEtat($clickedAction === 'publier' ? Etat::Ouverte : Etat::EnCreation);
+
+            //sauvegarde en bdd
+            $em->flush();
+            //affichage d'un message sur la prochaine page pour confirmation la modification
+            $this->addFlash('success', $clickedAction === 'publier'
+                ? 'Sortie publiée avec succès!'
+                : 'Sortie enregistrée en tant que brouillon.');
+            //redirection vers la page de détail de la sortie tout juste modifiée
+            return $this->redirectToRoute('app_sortie_detail', ['id' => $sortie->getId()]);
+        }
+        return $this->render('sortie/create.html.twig', [
+            'sortieForm' => $sortieForm,
             'sortie' => $sortie,
         ]);
     }
